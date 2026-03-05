313522e17e49854038a16686afe46116aa5c4e97
karamo10 <pabimamie@gmail.com>
2026-03-05 23:46:25 +0000

    feat: add Booking and Contact pages with development notices

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,33 @@
+
+import Link from 'next/link';
+export default function Booking() {
+  return (
+    <section>
+      <div className="text-center py-20">
+        <p className="text-clr-secondary capitalize mb-2">
+          ⚠️ <br /> Booking page is under development
+        </p>
+        <Link
+          href={'/'}
+          className="inline-flex items-center gap-1 bg-black px-3.5 py-2 rounded text-white text-sm hover:opacity-80 transition-all duration-300 cursor-pointer"
+        >
+          <span>Return back</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </Link>
+      </div>
+    </section>
+  );
+}
