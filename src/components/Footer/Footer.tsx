4262c619ff320300156c2ba75b22209b1ca2696f
karamo10 <pabimamie@gmail.com>
2026-03-05 23:48:06 +0000

    feat: update code

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <section id="contact" className="py-16">
+      <div>
+        <p className="text-center text-gray-500 mt-6">
+          &copy; 2026 FaksTours.com
+        </p>
+      </div>
+    </section>
+  );
+}
