579bebf676da4c9ac8b487381ae643aaa17c1ee0
karamo10 <pabimamie@gmail.com>
2026-03-05 23:45:47 +0000

    feat: update TourSlider component with new tour data and responsive styles

--- src/components/TourSlider/TourSlider.tsx
+++ src/components/TourSlider/TourSlider.tsx
@@ -1,37 +1,35 @@
 'use client';
 
-import Link from 'next/link';
 import Image from 'next/image';
 
 const tours = [
   {
-    name: 'Fathala Safari Park',
-    slug: 'fathala-safari-park',
-    image: '/images/galleries/fathala/fathala.jpg',
-  },
-  {
     name: 'Makasutu Cultural Forest',
-    slug: 'makasutu-forest',
-    image: '/images/galleries/makasutu/makasutu.jpg',
+    image: '/images/galleries/makasutu/1.jpg',
   },
   {
-    name: 'River Excursion',
-    slug: 'river-excursion',
-    image: '/images/galleries/river-gambia/river-gambia.jpg',
+    name: 'River Gambia',
+    image: '/images/galleries/river-gambia/1.jpg',
+  },
+  {
+    name: 'Fathala Wildlife Reserve',
+    image: '/images/galleries/fathala/1.jpg',
+  },
+  {
+    name: 'Roots (James Island / Jufureh)',
+    image: '/images/galleries/roots/1.jpg',
   },
 ];
 
 export default function TourSlider() {
   return (
-    <section className="py-1 overflow-hidden bg-red-00">
-      {/* <h2 className="text-3xl font-bold mb-6 px-6 capitalize">Explore top destination</h2> */}
-      {/* Tours & Experiences */}
+    <section className="py-1 overflow-hidden overflow-x-auto custom-scrollbar bg-red-00">
 
       <div className="flex gap-6 animate-scroll group-hover:[animation-play-state:paused] px-6 auto-s">
         {tours.concat(tours).map((tour, i) => (
           <div
             key={i}
-            className="group relative min-w-[420px] h-[380px] rounded-xl overflow-hidden cursor-pointer"
+            className="group relative min-w-[300px] h-[380px] md:min-w-[420px] rounded-xl overflow-hidden cursor-pointer"
           >
             <Image
               src={tour.image}
@@ -42,10 +40,7 @@ export default function TourSlider() {
 
             <div className="absolute inset-0 bg-black/40 flex items-end p-6">
               <div className="flex justify-between items-center w-full text-white">
-                <h3 className="text-xl font-semibold">{tour.name}</h3>
-                <span className="text-2xl transition group-hover:translate-x-1">
-                  →
-                </span>
+                <h3 className="text-sm font-semibold">{tour.name}</h3>
               </div>
             </div>
           </div>
